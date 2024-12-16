d6e24e7b4eb576d42c902858c62041e1c030fb03
Jordan Davis <jordantdavis@users.noreply.github.com>
2024-12-15 23:07:14 -0600

    Add basic elixir-mode support.

---
+++ lisp/init-elixir.el
@@ -0,0 +1,11 @@
+;;; init-elixir.el --- Support for the Elixir language -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(maybe-require-package 'elixir-mode)
+
+;; TODO: configure copilot for Elixir
+;; (add-to-list 'copilot-major-mode-alist '("elixir-mode" . "elixir"))
+
+(provide 'init-elixir)
+;;; init-elixir.el ends here
